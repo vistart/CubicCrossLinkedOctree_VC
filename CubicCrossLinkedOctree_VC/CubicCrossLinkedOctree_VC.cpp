0708e1d9a4c01c569c72c4afc9fa54228b9972fe
Zhao vistart <i@vistart.me>
2019-12-07 21:28:58 +0800

    seperate the plyfile into individual project.

--- CubicCrossLinkedOctree_VC/CubicCrossLinkedOctree_VC.cpp
+++ CubicCrossLinkedOctree_VC/CubicCrossLinkedOctree_VC.cpp
@@ -25,6 +25,7 @@ int main(int argc, char* argv[])
 	string file_path(argv[1]);
 	unique_ptr<PlyFile> plyfile = make_unique<PlyFile>(file_path);
     //cout << "Hello World!\n";
+    return 0;
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
