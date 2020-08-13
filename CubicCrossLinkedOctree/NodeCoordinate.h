8aa205abaf26651589641a209e59290b5ab91f13
Zhao vistart <i@vistart.me>
2020-08-13 16:17:06 +0800

    Abandon the design pattern of separation of attributes and fields.

--- CubicCrossLinkedOctree/NodeCoordinate.h
+++ CubicCrossLinkedOctree/NodeCoordinate.h
@@ -19,42 +19,10 @@ class NodeCoordinate
 {
 public:
     NodeCoordinate() = default;
-	NodeCoordinate(unsigned int X, unsigned int Y, unsigned int Z, unsigned char depth) : __X(X), __Y(Y), __Z(Z), __depth(depth)
+	NodeCoordinate(unsigned int X, unsigned int Y, unsigned int Z, unsigned char depth) : X(X), Y(Y), Z(Z), depth(depth)
 	{
 	}
 	~NodeCoordinate() = default;
-    [[nodiscard]] unsigned int X() const noexcept
-    {
-        return __X;
-    }
-    virtual NodeCoordinate& X(unsigned int const X) noexcept {
-        __X = X;
-        return *this;
-    }
-    [[nodiscard]] unsigned int Y() const noexcept
-    {
-        return __Y;
-    }
-    virtual NodeCoordinate& Y(unsigned int const Y) noexcept {
-        __Y = Y;
-        return *this;
-    }
-    [[nodiscard]] unsigned int Z() const noexcept
-    {
-        return __Z;
-    }
-    virtual NodeCoordinate& Z(unsigned int const Z) noexcept {
-        __Z = Z;
-        return *this;
-    }
-    [[nodiscard]] unsigned char depth() const noexcept
-    {
-        return __depth;
-    }
-    virtual NodeCoordinate& depth(unsigned char const depth) noexcept {
-        __depth = depth;
-        return *this;
-    }
 
     virtual bool operator==(NodeCoordinate const& node_coordinate) const;
 
@@ -74,13 +42,13 @@ public:
 		 */
 		size_t operator()(NodeCoordinate const& i) const
 		{
-            return static_cast<size_t>(i.__depth) << 57 ^
-                   static_cast<size_t>(i.__X & 0x7FFFF) << 38 ^
-                   static_cast<size_t>(i.__Y & 0x7FFFF) << 19 ^
-                   static_cast<size_t>(i.__Z & 0x7FFFF);
+            return static_cast<size_t>(i.depth) << 57 ^
+                   static_cast<size_t>(i.X & 0x7FFFF) << 38 ^
+                   static_cast<size_t>(i.Y & 0x7FFFF) << 19 ^
+                   static_cast<size_t>(i.Z & 0x7FFFF);
 		}
 	};
-
+    /*
 	struct Equal
 	{
 		bool operator()(NodeCoordinate const& left, NodeCoordinate const& right) const
@@ -88,11 +56,11 @@ public:
             return left == right;
 		}
 	};
+	*/
 
-protected:
-    unsigned int __X = 0;
-    unsigned int __Y = 0;
-    unsigned int __Z = 0;
-    unsigned char __depth = 1;
+    unsigned int X = 0;
+    unsigned int Y = 0;
+    unsigned int Z = 0;
+    unsigned char depth = 1;
 };
 #endif
